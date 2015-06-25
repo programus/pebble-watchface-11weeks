61e51f207cee00977faf6da082ffbad14dfe655e
Programus <programus@gmail.com>
2015-06-25 22:38:00 +0900

    Fixed the number display mistakes.

--- src/calendar_layer.c
+++ src/calendar_layer.c
@@ -82,24 +82,25 @@ static void update_bg_buffer(GContext* ctx) {
   memcpy(s_bg_buffer, frame_buffer, s_bg_buffer_size * sizeof(buffer_t));
   graphics_release_frame_buffer(ctx, bmp);
   
-  char* buff = (char*) malloc(s_bg_size.w);
+//   // log all frame buffer
+//   char* buff = (char*) calloc(sizeof(char), s_bg_size.w + 1);
   
-  int index = 0;
-  for (int y = 0; y < s_bg_size.h; y++) {
-    for (int x = 0; x < s_bg_size.w; x++) {
-      int di = x >> 3;
-      int shift = x & 07;
-      buffer_t mask = 0x80 >> shift;
-      if (s_bg_buffer[index + di] & mask) {
-        buff[x] = 'X';
-      } else {
-        buff[x] = '.';
-      }
-    }
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%3d, %4d: %s", y, index, buff);
-    index += s_bg_row_size_byte;
-  }
-  free(buff);
+//   int index = 0;
+//   for (int y = 0; y < s_bg_size.h; y++) {
+//     for (int x = 0; x < s_bg_size.w; x++) {
+//       int di = x >> 3;
+//       int shift = x & 07;
+//       buffer_t mask = 1 << shift;
+//       if ((s_bg_buffer[index + di] & mask) == mask) {
+//         buff[x] = 'X';
+//       } else {
+//         buff[x] = '.';
+//       }
+//     }
+//     APP_LOG(APP_LOG_LEVEL_DEBUG, "%3d: %144s", y, buff);
+//     index += s_bg_row_size_byte;
+//   }
+//   free(buff);
 }
 
 static void destroy_bg_buffer() {
@@ -114,7 +115,7 @@ static uint8_t get_pixel_from_buffer(int x, int y) {
 #ifdef PBL_BW
   int index = y * (s_bg_row_size_byte) + (x >> 3);
   buffer_t shift = x & 0x07;
-  buffer_t mask = 0x80 >> shift;
+  buffer_t mask = 1 << shift;
   APP_LOG(APP_LOG_LEVEL_DEBUG, "(%d, %d) => %d, %x", x, y, index, mask);
   return (s_bg_buffer[index] & mask) ? 1 : 0;
 #elif PBL_COLOR
