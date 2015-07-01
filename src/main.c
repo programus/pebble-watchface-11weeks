4768a64870930258644b40c2dea9f11c2d8db465
programus <programus@gmail.com>
2015-07-01 23:00:24 +0900

    added watch battery layer.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include <pebble.h>
 #include "main.h"
-#include "calendar_layer.h"
-#include "sec_layer.h"
+
 #include "numbers.h"
 #include "letters.h"
-  
+
+#include "calendar_layer.h"
+#include "sec_layer.h"
+#include "watch_battery_layer.h"
+
 static Window* s_main_window;
 static Layer* s_calendar_layer;
 static Layer* s_sec_layer;
+static Layer* s_watch_battery_layer;
 
 static time_t s_now_t;
 static struct tm s_now_tm;
@@ -18,6 +22,7 @@ static void main_window_load(Window*);
 static void main_window_unload(Window*);
 static void tick_handler(struct tm*, TimeUnits);
 static void update_time(bool is_init);
+static void battery_handler(BatteryChargeState charge_state);
 
 static void init() {
   // init numbers
@@ -36,14 +41,14 @@ static void init() {
   // set background color
   window_set_background_color(s_main_window, GColorBlack);
   
-  // show window on the watch, with animated = true
-  window_stack_push(s_main_window, true);
-  
-  // display time from the beginning
-  update_time(true);
-  
   // register the time tick service
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  
+  // register the battery service
+  battery_state_service_subscribe(battery_handler);
+  
+  // show window on the watch, with animated = true
+  window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
@@ -61,6 +66,15 @@ static void main_window_load(Window* window) {
   sec_layer_create();
   s_sec_layer = sec_layer_get_layer();
   layer_add_child(window_get_root_layer(s_main_window), s_sec_layer);
+  // create watch battery layer
+  watch_battery_layer_create();
+  s_watch_battery_layer = watch_battery_layer_get_layer();
+  layer_add_child(window_get_root_layer(s_main_window), s_watch_battery_layer);
+  
+  // display time from the beginning
+  update_time(true);
+  // display battery status from the beginning
+  battery_handler(battery_state_service_peek());
 }
 
 static void main_window_unload(Window* window) {
@@ -68,6 +82,8 @@ static void main_window_unload(Window* window) {
   calendar_layer_destroy();
   // destroy sec layer
   sec_layer_destroy();
+  // destroy watch battery layer
+  watch_battery_layer_destroy();
 }
 
 static void tick_handler(struct tm* tick_time, TimeUnits units_changed) {
@@ -87,6 +103,11 @@ static void update_time(bool is_init) {
     calendar_layer_update_time(&s_now_t, &s_now_tm);
     layer_mark_dirty(s_calendar_layer);
   }
+}
+
+static void battery_handler(BatteryChargeState charge_state) {
+  watch_battery_layer_update(charge_state);
+  layer_mark_dirty(s_watch_battery_layer);
 }
 
 int main(void) {
---
+++ src/watch_battery_layer.h
@@ -0,0 +1,19 @@
+//
+//  watch_battery_layer.h
+//  pebble-watchface-months
+//
+//  Created by Programus on 15/7/1.
+//
+//
+
+#ifndef __pebble_watchface_months__watch_battery_layer__
+#define __pebble_watchface_months__watch_battery_layer__
+
+#include <pebble.h>
+
+void watch_battery_layer_create();
+void watch_battery_layer_destroy();
+Layer* watch_battery_layer_get_layer();
+void watch_battery_layer_update(BatteryChargeState state);
+
+#endif /* defined(__pebble_watchface_months__watch_battery_layer__) */
